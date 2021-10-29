ceb248b07fe8efa0353866af6fd1e77d6c52da31
DarkSunrise08 <88215000+DarkSunrise08@users.noreply.github.com>
2021-10-29 15:44:34 -0700

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,21 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonBase = loadImage("./assets/cannonBase.png");
+    this.canon = loadImage("./assets/canon.png");    
+  }
+  
+  display(){
+    push();
+    translate(this.x,this.y);
+    rotate(this.angle);
+    imageMode(CENTER);
+    image(this.canon,0,0,this.width,this.height);  
+    pop();
+    image(this.cannonBase,this.x-115,this.y-100,this.width+10,this.height+125);  
+  }
+}
